85f8a03cffd9e6289ae144c8e3bc837e2aad7e18
benhus8 <benedykt.huszcza@gmail.com>
2023-05-12 21:22:06 +0200

    initial commit

---
+++ app/src/main/java/com/inf151313/boardgamecollector/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.inf151313.boardgamecollector
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
